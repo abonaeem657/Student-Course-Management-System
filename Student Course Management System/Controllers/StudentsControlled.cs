42b446f371044e657a550451178f97f03a3b2f0b
Abo_Naeem <mhmdnymalshryf2@gmail.com>
2026-09-23 12:05:27 +0300

    Initial ASP.NET MVC project

---
+++ Student Course Management System/Controllers/StudentsControlled.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student_Course_Management_System.Models;
+
+namespace Student_Course_Management_System.Controllers
+{
+    public class StudentsControlled : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            student.Id = 1;
+            student.Name = "Ali";
+            student.Major = "CS";
+            return View(student);
+        }
+        public IActionResult Detail()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id = 1, Name = "mohamed", Major = "CS" });
+            students.Add(new Student { Id = 2, Name = "naeem", Major = "AI" });
+            students.Add(new Student { Id = 3, Name = "ahmed", Major = "SI" });
+            return View(students);
+        }
+    }
+}
---
+++ Student Course Management System/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Student_Course_Management_System.Models
+{
+    public class Student
+    {
+       public int Id{ get; set; } 
+        public string Name { get; set; } 
+        public string Major { get; set; } 
+    }
+}
