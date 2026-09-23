8f7e2292ebdd03816a5e72c9c5d768392e470fe6
Abo_Naeem <mhmdnymalshryf2@gmail.com>
2026-09-23 21:32:17 +0300

    check now

--- Student Course Management System/Controllers/CourseController.cs
+++ Student Course Management System/Controllers/CourseController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Student_Course_Management_System.Models;
+using Student_Course_Management_System.Data;
 
 namespace Student_Course_Management_System.Controllers
 {
     public class CourseController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public CourseController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult Index()
+        {   List<Course> courses = _context.Courses.ToList();
+            return View(courses);
+        }
+        public IActionResult Details(int id)
+        {
+            Course? course= _context.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null) { return NotFound(); }
+            return View(course);
         }
     }
 }
