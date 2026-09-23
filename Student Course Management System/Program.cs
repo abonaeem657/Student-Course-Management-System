86bef3ce42f4047eca1ebeae6cf1d7ab1af47d81
Abo_Naeem <mhmdnymalshryf2@gmail.com>
2026-09-23 12:24:42 +0300

    Fix student controller and views names

--- Student Course Management System/Program.cs
+++ Student Course Management System/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Students}/{action=Detail}/{id?}");
+    pattern: "{controller=Student}/{action=Detail}/{id?}");
 
 app.Run();
